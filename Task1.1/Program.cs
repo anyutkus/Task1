270a3c7098729ca3cbe4385e0ff20b103aad4829
Anna Nedelkina <annanedelkina@MacBook-Pro.local>
2022-03-28 14:43:09 +0300

    first commit

---
+++ Task1.1/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * При старте приложение запрашивает у пользователя два целых числа a и b (считать, что пользователь вводит целые числа без ошибок). 
+ * Затем приложение выводит все целые числа в диапазоне от a (включительно) до b (включительно), 
+ * которые в своём троичном представлении имеют ровно две двойки. 
+ * Разработать консольное приложение, реализующее указанный функционал.
+ * Примечание: для преобразования строки s в значение типа int используйте метод-функцию int.Parse(s).
+*/
+
+using System;
+
+Console.Write("Input a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Input b = ");
+int b = int.Parse(Console.ReadLine());
+
+int temp = a;
+int a3 = 0;
+int k = 1;
+
+while (temp > 0) //converting into ternary numeral system
+{
+    a3 += (temp % 3) * k;
+    temp /= 3;
+    k *= 10;
+}
+
+while (a <= b)
+{
+    temp = a3;
+    amountOf2(temp);//counting the amount of 2 in a number
+    a3 = addOne(a3);//adding 1 to the number in ternary numeral system
+    a++;
+}
+
+void amountOf2(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        if (number % 10 == 2)
+        {
+            count++;
+        }
+        number /= 10;
+    }
+    if (count == 2)
+    {
+        Console.WriteLine($"{a}  {a3}");
+    }
+}
+
+int addOne(int number3)
+{
+    temp = number3;
+    for (int i = 10; i <= k; i *= 10)
+    {
+        if ((temp % 10 == 2) && (temp / 10 == 0))
+        {
+            number3 = k;
+            k *= 10;
+            return number3;
+        }
+        if (temp % 10 == 2)
+        {
+            number3 -= 2 * i / 10;
+        }
+        else
+        {
+            number3 += i / 10;
+            return number3;
+        }
+        temp /= 10;
+    }
+    return number3;
+}
---
+++ Task1.3/Program.cs
@@ -0,0 +1,59 @@
+﻿/*При старте приложение запрашивает у пользователя количество элементов в числовом массиве (минимум – два), а затем в цикле – сами элементы (типа int). 
+ * После ввода элементов приложение распечатывает исходный массив и выводит сумму элементов массива, 
+ * расположенных между наименьшим элементом массива (самым левым, если их несколько) и наибольшим элементом (самым правым, если их несколько). 
+ * Разработать консольное приложение, реализующее указанный функционал.
+ * Пример: массив [1,  3,  5,  1,  0,  3,  0,  1]. Сумма нужных элементов = 5 + 1 + 0 = 6.
+ * Примечание 1: корректность длины массива можно не контролировать.
+ * Примечание 2: наименьший и наибольший элементы включаются в сумму.
+ */
+
+using System;
+
+Console.Write("Input number of elements: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[n];
+
+int min, max, minPosition, maxPosition;
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Input element {i}: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+min = max = numbers[0];
+minPosition = maxPosition = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+        minPosition = i;
+    }
+    if (numbers[i] >= max)
+    {
+        max = numbers[i];
+        maxPosition = i;
+    }
+}
+
+calculate(minPosition, maxPosition);
+
+void calculate(int a, int b)
+{
+    int sum = 0;
+    if (a > b)
+    {
+        a += b;
+        b = a - b;
+        a -= b;
+    }
+    while (a <= b)
+    {
+        sum += numbers[a];
+        a++;
+    }
+    Console.WriteLine($"Sum = {sum}");
+}
